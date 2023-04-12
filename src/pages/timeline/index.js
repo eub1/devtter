14abc85ae11a16d6e62c4deb431cd7e7a6a41d12
eub1 <ebonforte@gmail.com>
2023-04-12 11:10:25 -0300

    create Timeline page

---
+++ src/pages/timeline/index.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function Timeline({ userName }) {
+	return (
+		<>
+			<h1>This is the timeline of {userName}</h1>
+			<div>
+				<Link href="/">Go Home</Link>
+			</div>
+			<style jsx>
+				{"h1, div {align-item: center; color: #09f;}, {h1 {font-size: 36px;}}"}
+			</style>
+		</>
+	);
+}
+
+Timeline.getInitialProps = () => {
+	return fetch("http://localhost:3000/api/hello")
+		.then((res) => res.json())
+		.then((response) => {
+			const { userName } = response;
+			return { userName };
+		});
+};
